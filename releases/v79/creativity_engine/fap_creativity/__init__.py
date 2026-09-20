b7d73bb29e9e4a9693b6f40f374d339c00649493
kobayashi-cyder <52078391+kobayashi-cyder@users.noreply.github.com>
2026-09-20 14:15:52 +0900

    Expose primitive invention loop API

--- releases/v79/creativity_engine/fap_creativity/__init__.py
+++ releases/v79/creativity_engine/fap_creativity/__init__.py
@@ -7,6 +7,17 @@ from .engine import (
     OPERATORS,
 )
 from .integration import build_v79_responder
+from .primitive_invention import (
+    Instruction,
+    MiniIRSandbox,
+    PrimitiveCandidate,
+    PrimitiveInventor,
+    PrimitivePromotionLoop,
+    PrimitiveTestCase,
+    PromotionDecision,
+    SandboxResult,
+    SkillRegistry,
+)
 
 __all__ = [
     "CreativeCandidate",
@@ -16,4 +27,13 @@ __all__ = [
     "CreativityEngine",
     "OPERATORS",
     "build_v79_responder",
+    "Instruction",
+    "MiniIRSandbox",
+    "PrimitiveCandidate",
+    "PrimitiveInventor",
+    "PrimitivePromotionLoop",
+    "PrimitiveTestCase",
+    "PromotionDecision",
+    "SandboxResult",
+    "SkillRegistry",
 ]
