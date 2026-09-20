702e63e19c796e8b0581ef9da70b84600698837c
kobayashi-cyder <52078391+kobayashi-cyder@users.noreply.github.com>
2026-09-20 13:35:24 +0900

    Export creativity experience validators

--- releases/v79/creativity_engine/fap_creativity/__init__.py
+++ releases/v79/creativity_engine/fap_creativity/__init__.py
@@ -6,6 +6,7 @@ from .engine import (
     CreativityEngine,
     OPERATORS,
 )
+from .experience_validation import validate_experience_file, validate_experience_payload
 from .integration import build_v79_responder
 
 __all__ = [
@@ -16,4 +17,6 @@ __all__ = [
     "CreativityEngine",
     "OPERATORS",
     "build_v79_responder",
+    "validate_experience_file",
+    "validate_experience_payload",
 ]
