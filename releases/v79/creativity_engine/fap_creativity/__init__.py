80c9b52438b343aa8b8b3a949f3f2d4775383b96
kobayashi-cyder <52078391+kobayashi-cyder@users.noreply.github.com>
2026-09-20 13:30:28 +0900

    V79: add __init__.py

---
+++ releases/v79/creativity_engine/fap_creativity/__init__.py
@@ -0,0 +1,17 @@
+from .engine import (
+    CreativeCandidate,
+    CreativeExperience,
+    CreativeExperienceStore,
+    CreativityAugmentedResponder,
+    CreativityEngine,
+    OPERATORS,
+)
+
+__all__ = [
+    "CreativeCandidate",
+    "CreativeExperience",
+    "CreativeExperienceStore",
+    "CreativityAugmentedResponder",
+    "CreativityEngine",
+    "OPERATORS",
+]
