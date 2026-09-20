468f96fa2bcc8394b53fd44d6c833421e01d1f76
kobayashi-cyder <52078391+kobayashi-cyder@users.noreply.github.com>
2026-09-20 14:21:51 +0900

    V81: integrate curriculum with V80 primitive sandbox

---
+++ releases/v81/self_curriculum/fap_self_curriculum/__init__.py
@@ -0,0 +1,38 @@
+from .engine import (
+    AbilityMap,
+    AbilityState,
+    AttemptResult,
+    CurriculumGenerator,
+    CurriculumTask,
+    PatternStore,
+    SelfCurriculumEngine,
+    SuccessCompressor,
+    SuccessPattern,
+    VerificationResult,
+)
+from .integration import FAPSelfCurriculum, build_v81_curriculum
+from .primitive_bridge import (
+    PRIMITIVE_ABILITIES,
+    PrimitiveExerciseGenerator,
+    PrimitiveSelfCurriculum,
+    build_primitive_self_curriculum,
+)
+
+__all__ = [
+    "AbilityMap",
+    "AbilityState",
+    "AttemptResult",
+    "CurriculumGenerator",
+    "CurriculumTask",
+    "PatternStore",
+    "SelfCurriculumEngine",
+    "SuccessCompressor",
+    "SuccessPattern",
+    "VerificationResult",
+    "FAPSelfCurriculum",
+    "build_v81_curriculum",
+    "PRIMITIVE_ABILITIES",
+    "PrimitiveExerciseGenerator",
+    "PrimitiveSelfCurriculum",
+    "build_primitive_self_curriculum",
+]
