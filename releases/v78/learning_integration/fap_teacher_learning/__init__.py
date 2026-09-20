613fe38c3fbfd5418ca556ebf4f695653e38896e
kobayashi-cyder <52078391+kobayashi-cyder@users.noreply.github.com>
2026-09-20 10:59:51 +0900

    Add V78 teacher-learning package exports

---
+++ releases/v78/learning_integration/fap_teacher_learning/__init__.py
@@ -0,0 +1,19 @@
+from .responder import (
+    CircuitActivation,
+    LearnedManagedChat,
+    LearningArtifactError,
+    LearningStatus,
+    TeacherLearningResponder,
+    TeacherLearningState,
+    build_interaction_runtime,
+)
+
+__all__ = [
+    "CircuitActivation",
+    "LearnedManagedChat",
+    "LearningArtifactError",
+    "LearningStatus",
+    "TeacherLearningResponder",
+    "TeacherLearningState",
+    "build_interaction_runtime",
+]
