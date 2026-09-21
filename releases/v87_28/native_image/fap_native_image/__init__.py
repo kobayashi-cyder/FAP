16ff1df95d4afdda13cdb06efeac658e65cf251b
kobayashi-cyder <52078391+kobayashi-cyder@users.noreply.github.com>
2026-09-21 20:53:16 +0900

    Export native image engine package

---
+++ releases/v87_28/native_image/fap_native_image/__init__.py
@@ -0,0 +1,9 @@
+from .engine import NativeImageEngine, NativeImageError, PromptPlan
+from .manager import NativeImageManager
+
+__all__ = [
+    "NativeImageEngine",
+    "NativeImageError",
+    "NativeImageManager",
+    "PromptPlan",
+]
