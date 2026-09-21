776a54861a5ff4d557a889c2579397a4dfd77a8b
kobayashi-cyder <52078391+kobayashi-cyder@users.noreply.github.com>
2026-09-21 19:38:11 +0900

    Add V87.23 compact offline distilled image path

---
+++ releases/v87_23/compact_offline/fap_compact_offline/__init__.py
@@ -0,0 +1,13 @@
+from .single_file import (
+    CompactOfflineImageEngine,
+    CompactOfflineManifest,
+    CompactOfflineError,
+    discover_compact_models,
+)
+
+__all__ = [
+    "CompactOfflineImageEngine",
+    "CompactOfflineManifest",
+    "CompactOfflineError",
+    "discover_compact_models",
+]
