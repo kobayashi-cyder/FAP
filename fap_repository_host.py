b63c69d88ad70226925f97f7ce4ed3b7746e45dc
kobayashi-cyder <52078391+kobayashi-cyder@users.noreply.github.com>
2026-09-23 18:36:34 +0900

    Validate host response serialization contract

--- fap_repository_host.py
+++ fap_repository_host.py
@@ -10,7 +10,7 @@ from fap_repository_agent import (
     RepairProvider,
     RepositoryCodingCoordinator,
 )
-from fap_repository_contracts import HOST_CONTRACT
+from fap_repository_contracts import HOST_CONTRACT, validate_contract_payload
 from fap_repository_verifier import VerificationCommand
 
 
