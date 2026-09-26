350f3f4e93501a4fc214f2ecb5bf9bfa7f36f401
kobayashi-cyder <52078391+kobayashi-cyder@users.noreply.github.com>
2026-09-26 18:56:39 +0900

    native(r006): bump native response variant

--- native_cpp/include/fap/native_core.hpp
+++ native_cpp/include/fap/native_core.hpp
@@ -8,7 +8,7 @@
 
 namespace fap::cppcore {
 
-inline constexpr const char* kVersion = "1.0.01-cpp-native-r005";
+inline constexpr const char* kVersion = "1.0.01-cpp-native-r006";
 
 struct Signal {
     std::string kind;
